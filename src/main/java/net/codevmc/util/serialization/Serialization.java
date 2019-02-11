196af419309e33d3ff03e6f661bebb706485901a
defoliation <defoli_ation@outlook.com>
2019-02-11 13:32:02 +0800

    add javadoc

--- src/main/java/net/codevmc/util/serialization/Serialization.java
+++ src/main/java/net/codevmc/util/serialization/Serialization.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * a simple field annotation Serialization.
+ * only annotation field.
+ * class must have a empty constructor.
+ *
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Serialization {
